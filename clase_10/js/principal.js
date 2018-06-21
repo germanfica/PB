b3d4982fcc4c8d5701434dbce9a247f182d67bae
German Fica <germafica@gmail.com>
2018-06-21 17:56:43 -0300

    New update

---
+++ clase_10/js/principal.js
@@ -0,0 +1,4 @@
+var ruta = window.location; // Location es un Atributo o propiedad del objecto window
+// Console es un objeto.
+console.log(ruta); // Súper poder para encontrar cómo está escrito un objecto por dentro y scarle todos los datos interesantes que necesitemos
+document.write("Hola estudiante. Estás en la ruta " + ruta.origin);
