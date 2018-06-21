4c8fb5e64e1b2d99f6ce8581ac8b2730a77d9a24
German Fica <germafica@gmail.com>
2018-06-21 13:48:39 -0300

    New update

--- clase_9/js/utiles.js
+++ clase_9/js/utiles.js
@@ -1,3 +1,7 @@
-function peso(pTierra, g) {
+/**
+ * Retorna el peso en función del peso elejido y la gravedad del planeta,
+ * seleccionado.
+ */
+function pesoPlaneta(pTierra, g) {
 	return pTierra * g / gTierra; // Retorna el peso en marte
 }
