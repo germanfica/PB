b4fb60e0ad498e793da54858cb4c788d2648e894
German Fica <germafica@gmail.com>
2018-06-27 14:30:40 -0300

    New update

--- clase_16/js/principal.js
+++ clase_16/js/principal.js
@@ -1,7 +1,10 @@
 // Declaración de variables
-var teclas;
+var dibujo, lienzo, teclas, alturaDibujo;
 
 // Inicialización de variables
+dibujo = document.getElementById("area_de_dibujo");
+lienzo = dibujo.getContext("2d"); // Área de dibujo, el lienzo es nuestro papel
+alturaDibujo = dibujo.height;
 teclas = {
  UP: 38,
  DOWN: 40,
@@ -16,7 +19,11 @@ document.addEventListener("keyup", dibujarTeclado);
 function dibujarTeclado(evento) {
   switch (evento.keyCode) {
     case teclas.UP:
-      console.log(evento.key);
+      //console.log(evento.key);
+      dibujarLinea("black", 1, 1, 1, 80, 1);
+      //dibujarLinea("black", 1, 1, alturaDibujo-1, 1, 1);
+      //dibujarLinea("black", alturaDibujo-1, 1, alturaDibujo-1, alturaDibujo-1, 1);
+      //dibujarLinea("black", alturaDibujo-1, alturaDibujo-1, 1, alturaDibujo-1, 1);
       break;
     case teclas.DOWN:
       console.log(evento.key);
@@ -31,4 +38,16 @@ function dibujarTeclado(evento) {
     break;
   }
   //console.log(evento.keyCode);
+}
+
+function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal, lineWidth) {
+  lienzo.beginPath();
+    // Comienzo
+    lienzo.lineWidth = lineWidth; // Define el grosor de la línea.
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xInicial, yInicial); // x, y
+  	lienzo.lineTo(xFinal, yFinal);
+  	lienzo.stroke();
+    // Cierre
+  lienzo.closePath();
 }
