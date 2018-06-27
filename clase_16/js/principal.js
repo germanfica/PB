036cc812f3047ede5bbfad6b52110ad97d108afb
German Fica <germafica@gmail.com>
2018-06-27 14:16:02 -0300

    New update

--- clase_16/js/principal.js
+++ clase_16/js/principal.js
@@ -1,35 +1,34 @@
 // Declaración de variables
-var teclas = {
-  UP: 38,
-  DOWN: 40,
-  LEFT: 37,
-  RIGHT: 39
-};
-
-// 27
+var teclas;
 
 // Inicialización de variables
+teclas = {
+ UP: 38,
+ DOWN: 40,
+ LEFT: 37,
+ RIGHT: 39
+};
 
 // Detectar tecla y ejecutar una función
 document.addEventListener("keyup", dibujarTeclado);
 
 // addEventListener almacena toda la información en 'evento'
 function dibujarTeclado(evento) {
-  // Arriba
-  if(evento.keyCode == teclas.UP) {
-    console.log(evento.key);
-  }
-  // Abajo
-  if(evento.keyCode == teclas.DOWN) {
-    console.log(evento.key);
-  }
-  // Izquierda
-  if(evento.keyCode == teclas.LEFT) {
-    console.log(evento.key);
-  }
-  // Derecha
-  if(evento.keyCode == teclas.RIGHT) {
-    console.log(evento.key);
+  switch (evento.keyCode) {
+    case teclas.UP:
+      console.log(evento.key);
+      break;
+    case teclas.DOWN:
+      console.log(evento.key);
+      break;
+    case teclas.LEFT:
+      console.log(evento.key);
+      break;
+    case teclas.RIGHT:
+      console.log(evento.key);
+      break;
+    default:
+    break;
   }
   //console.log(evento.keyCode);
 }
