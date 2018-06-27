b34864c562c42df0dad7b8c3b48f5e77d4ea05d9
German Fica <germafica@gmail.com>
2018-06-27 13:40:51 -0300

    New update

--- clase_16/js/principal.js
+++ clase_16/js/principal.js
@@ -1,5 +1,35 @@
-// Varaibles declaration
-var canvas;
+// Declaración de variables
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
 
-// Variables initialization
-canvas = document.getElementById('canvas');
+// 27
+
+// Inicialización de variables
+
+// Detectar tecla y ejecutar una función
+document.addEventListener("keyup", dibujarTeclado);
+
+// addEventListener almacena toda la información en 'evento'
+function dibujarTeclado(evento) {
+  // Arriba
+  if(evento.keyCode == teclas.UP) {
+    console.log(evento.key);
+  }
+  // Abajo
+  if(evento.keyCode == teclas.DOWN) {
+    console.log(evento.key);
+  }
+  // Izquierda
+  if(evento.keyCode == teclas.LEFT) {
+    console.log(evento.key);
+  }
+  // Derecha
+  if(evento.keyCode == teclas.RIGHT) {
+    console.log(evento.key);
+  }
+  //console.log(evento.keyCode);
+}
