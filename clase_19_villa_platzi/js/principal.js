b03c453d86e52b33a10d3b1e12760899b7c47b04
German Fica <germafica@gmail.com>
2018-06-28 21:46:56 -0300

    New update

--- clase_19_villa_platzi/js/principal.js
+++ clase_19_villa_platzi/js/principal.js
@@ -1,6 +1,51 @@
-// Mostrar por pantalla la nueva posición
-for (var i = 0; i <= 10; i++) {
-  document.write("La nueva posición generada es: " + aleatorio(0, 500) + "<br/>");
+// Declaración de variables
+var vp, lienzo, fondoDibujado;
+
+// Incicialización de variables
+ vp = document.getElementById("villaplatzi");
+ lienzo = vp.getContext("2d");
+ fondoDibujado = false;
+
+var vaca = {
+  url: "vaca.png",
+  cargaOK: false
+};
+
+// Creación del fondo
+var fondo = new Image();
+fondo.src = "images/tile.png";
+fondo.addEventListener("onload", dibujarFondo);
+
+// Creación de la vaca
+var vaca = new Image();
+vaca.src = "images/vaca.png"
+vaca.addEventListener("load", dibujarVaca);
+
+// Creación del cerdo
+var cerdo = new Image();
+cerdo.src = "images/cerdo.png"
+cerdo.addEventListener("load", dibujarCerdo);
+
+// Creación del pollo
+var pollo = new Image();
+pollo.src = "images/pollo.png"
+pollo.addEventListener("load", dibujarPollo);
+
+function dibujarFondo(evento) {
+  lienzo.drawImage(fondo, 0, 0);
+  fondoDibujado = true;
+}
+
+function dibujarVaca() {
+  lienzo.drawImage(vaca, aleatorio(0,420), aleatorio(0,420));
+}
+
+function dibujarCerdo() {
+  lienzo.drawImage(cerdo, aleatorio(0,420), aleatorio(0,420));
+}
+
+function dibujarPollo() {
+  lienzo.drawImage(pollo, aleatorio(0,420), aleatorio(0,420));
 }
 
 /**
