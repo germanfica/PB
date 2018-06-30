72fd442c9d9db5ff467a2fe6a7e45d2c8380bdf1
German Fica <germafica@gmail.com>
2018-06-30 01:25:04 -0300

    New update

--- clase_19_villa_platzi_desafio/js/movimiento.js
+++ clase_19_villa_platzi_desafio/js/movimiento.js
@@ -11,8 +11,8 @@ teclas = {
  LEFT: 37,
  RIGHT: 39
 };
-x = 150;
-y = 150;
+x = aleatorio(0,420);
+y = aleatorio(0,420);
 
 // Dibujar al personaje
 document.addEventListener("keyup", dibujarPersonaje);
@@ -21,30 +21,35 @@ function dibujarPersonaje(evento) {
   var color = "brown";
   var movimiento = 10;
 
-  // Limpieza del lienzo
-  lienzoPersonaje.clearRect(0, 0, personaje.width, personaje.height);
+  // Crear personaje
+  if(cerdo.cargaOK)
+    lienzoPersonaje.drawImage(cerdo.imagen, x, y);
 
   switch (evento.keyCode) {
     case teclas.UP:
       //console.log(evento.key);
+      limpiarLienzoPersonaje();
       if(cerdo.cargaOK)
         lienzoPersonaje.drawImage(cerdo.imagen, x, y-movimiento);
       //dibujarLinea(color, x, y, x, y-movimiento, 3);
       y=y-movimiento;
       break;
     case teclas.DOWN:
+      limpiarLienzoPersonaje();
       if(cerdo.cargaOK)
         lienzoPersonaje.drawImage(cerdo.imagen, x, y+movimiento);
       //dibujarLinea(color, x, y, x, y+movimiento, 3);
       y=y+movimiento;
       break;
     case teclas.LEFT:
+      limpiarLienzoPersonaje();
       if(cerdo.cargaOK)
         lienzoPersonaje.drawImage(cerdo.imagen, x-movimiento, y);
       //dibujarLinea(color, x, y, x-movimiento, y, 3);
       x=x-movimiento;
       break;
     case teclas.RIGHT:
+      limpiarLienzoPersonaje();
       if(cerdo.cargaOK)
         lienzoPersonaje.drawImage(cerdo.imagen, x+movimiento, y);
       //dibujarLinea(color, x, y, x+movimiento, y, 3);
@@ -66,4 +71,12 @@ function dibujarLinea(color, xInicial, yInicial, xFinal, yFinal, lineWidth) {
   	lienzoPersonaje.stroke();
     // Cierre
   lienzoPersonaje.closePath();
+}
+
+/**
+  * Objetivo: limpiar el canvas del personaje.
+  *
+*/
+function limpiarLienzoPersonaje() {
+  lienzoPersonaje.clearRect(0, 0, personaje.width, personaje.height);
 }
