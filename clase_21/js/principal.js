c527254dc0812154486e2bc8d34e602fa5696f8c
German Fica <germafica@gmail.com>
2018-06-30 20:55:56 -0300

    New update

--- clase_21/js/principal.js
+++ clase_21/js/principal.js
@@ -2,8 +2,10 @@
 
 // Inicialización de variables
 
-// Crear Pakiman
+// Crear Pakimanes
 var cauchin = new Pakiman("Cauchin", 100, 80);
+var pokacho = new Pakiman("Pokacho", 80, 50);
+var vegenauro = new Pakiman("Vegenauro", 120, 40);
 
 document.write("Nombre: "+ cauchin.nombre + "<br/>");
 document.write("Vida: "+ cauchin.vida + "<br/>");
