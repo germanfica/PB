9120bc5bb245ac975b7951d08719ab056304b5d3
German Fica <germafica@gmail.com>
2018-06-30 20:20:51 -0300

    New update

--- clase_20_fizzbuzz/js/principal.js
+++ clase_20_fizzbuzz/js/principal.js
@@ -5,14 +5,29 @@ var max;
 max=100;
 
 for (var i = 1; i < max; i++) {
-  if(i%3==0) {
-    document.write("Fizz");
-  }else if (i%5==0) {
-    document.write("Buzz");
-  }else if (i%3==0 && i%5==0) {
+
+  if(esDivisible(i,3)==true && esDivisible(i,5)==true) {
     document.write("FizzBuzz");
+  }else if (esDivisible(i,3)==true) {
+    document.write("Fizz");
+  }else if (esDivisible(i,5)==true) {
+    document.write("Buzz");
   }else {
     document.write(i);
   }
   document.write("<br/>");
+}
+
+function esDivisible(numero, divisor) {
+  // Declaración de variables
+  var resultado;
+
+  // Inicialización de variables
+  resultado = false;
+
+  if(numero%divisor==0) {
+    resultado=true;
+  }
+
+  return resultado;
 }
