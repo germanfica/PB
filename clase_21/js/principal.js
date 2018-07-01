5654f5f54d4cdecfeb94d99fccdcd73c8cbf7534
German Fica <germafica@gmail.com>
2018-06-30 21:27:27 -0300

    New update

--- clase_21/js/principal.js
+++ clase_21/js/principal.js
@@ -1,11 +1,17 @@
 // Declaración de variables
+var imagenes = [];
 
 // Inicialización de variables
 
 // Crear Pakimanes
 var cauchin = new Pakiman("Cauchin", 100, 80);
+imagenes["Cauchin"] = "images/vaca.png";
+
 var pokacho = new Pakiman("Pokacho", 80, 50);
+imagenes["Pokacho"] = "images/pollo.png";
+
 var ceuro = new Pakiman("Ceuro", 120, 40);
+imagenes["Ceuro"] = "images/cerdo.png";
 
 document.write("Nombre: "+ cauchin.nombre + "<br/>");
 document.write("Vida: "+ cauchin.vida + "<br/>");
