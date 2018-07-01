64ef2755e66949801cb23d6171174f519e4a9871
German Fica <germafica@gmail.com>
2018-06-30 22:38:54 -0300

    New update

--- clase_21/js/principal.js
+++ clase_21/js/principal.js
@@ -1,12 +1,14 @@
-// Crear Pakimanes
+// Crear Pakiman Cauchin
 var cauchin = new Pakiman("Cauchin", 100, 80);
 cauchin.cargarImagen("images/vaca.png");
 cauchin.mostrar();
 
+// Crear Pakiman Pokacho
 var pokacho = new Pakiman("Pokacho", 80, 50);
 pokacho.cargarImagen("images/pollo.png");
 pokacho.mostrar();
 
+// Crear Pakiman Ceuro
 var ceuro = new Pakiman("Ceuro", 120, 40);
 ceuro.cargarImagen("images/cerdo.png");
 ceuro.mostrar();
