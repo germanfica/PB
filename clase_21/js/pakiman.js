4bee8480c7728900f4075b18abafb66ec359a100
German Fica <germafica@gmail.com>
2018-06-30 21:37:33 -0300

    New update

--- clase_21/js/pakiman.js
+++ clase_21/js/pakiman.js
@@ -1,8 +1,20 @@
+var imagenes = [];
+//imagenes["Cauchin"] = "images/vaca.png";
+//imagenes["Pokacho"] = "images/pollo.png";
+//imagenes["Ceuro"] = "images/cerdo.png";
+
 class Pakiman {
   constructor(nombre, vida, ataque) {
+    this.imagen = new Image();
     this.nombre = nombre;
     this.vida = vida;
     this.ataque = ataque;
+
+    //this.imagen.src = imagenes[this.nombre];
+  }
+
+  cargarImagen(url) {
+    this.imagen.src = url;
   }
 
   hablar() {
--- clase_21/js/principal.js
+++ clase_21/js/principal.js
@@ -1,17 +1,10 @@
-// Declaración de variables
-var imagenes = [];
-
-// Inicialización de variables
-
 // Crear Pakimanes
 var cauchin = new Pakiman("Cauchin", 100, 80);
-imagenes["Cauchin"] = "images/vaca.png";
-
+cauchin.cargarImagen("images/vaca.png");
 var pokacho = new Pakiman("Pokacho", 80, 50);
-imagenes["Pokacho"] = "images/pollo.png";
-
+pokacho.cargarImagen("images/pollo.png");
 var ceuro = new Pakiman("Ceuro", 120, 40);
-imagenes["Ceuro"] = "images/cerdo.png";
+ceuro.cargarImagen("images/cerdo.png");
 
 document.write("Nombre: "+ cauchin.nombre + "<br/>");
 document.write("Vida: "+ cauchin.vida + "<br/>");
