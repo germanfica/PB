a4fd13cd72be94ab3ab01df98982ffe903668168
German Fica <germafica@gmail.com>
2018-07-03 18:52:58 -0300

    New update

--- clase_22/js/principal.js
+++ clase_22/js/principal.js
@@ -14,9 +14,19 @@ function retirar(monto) {
   while (i<=billetes.length-1) {
     var billete = billetes[i][0];
     var cantBilletes = billetes[i][1];
+    var cantBilletesNec = monto/billete; // Catidad de billetes necesarios
 
-    console.log(cantBilletes + " billetes de " + billete + ".")
-    
+    console.log(monto); // Monto que seleccionó el cliente para retirar
+    console.log(monto/billete);
+    console.log(monto%billete); // Dinero restante por dar al cliente
+
+    // Si la cantidad de biletes necesarios es menos a la cantidad de billetes actuales, entonces esta ecuación determina el residuo de lo que falta para completar el monto seleccionado
+
+    if(cantBilletes>=cantBilletesNec) {
+      monto = monto%billete; // Dinero restante por dar al cliente
+    }
+
+    //console.log(cantBilletes + " billetes de " + billete + ".")
     i++;
   }
 }
