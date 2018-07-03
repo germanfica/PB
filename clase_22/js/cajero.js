6a2de99583a629302cc48be9608aa07546928152
German Fica <germafica@gmail.com>
2018-07-03 20:50:30 -0300

    New update TODO FUNCIONA :D

--- clase_22/js/cajero.js
+++ clase_22/js/cajero.js
@@ -7,9 +7,20 @@ class Cajero {
     ];
   }
 
+  /**
+    * Reducir la cantidad de billetes en el cajero
+    */
+  modificarCantBilletes(cantUsada) {
+    for (var i = 0; i <= cantUsada.length-1; i++) {
+      this.billetes[i][1] = (this.billetes[i][1]) - (cantUsada[i]);
+    }
+    //this.billetes[i][1] = this.billetes[i][1] - cantidadUsada;
+  }
+
   retirar(monto) {
     // Declaración de variables
     var i, billete, cantBilletes, cantBilletesReq, dineroRestante, todoOK;
+    var cantBilletesUsad = [];
 
     // Inicialización de variables
     i=0;
@@ -19,14 +30,19 @@ class Cajero {
     while (i<=this.billetes.length-1 && !todoOK) {
       billete = this.billetes[i][0]; // Billete del cajero
       cantBilletes = this.billetes[i][1]; // Cantidad de billetes del cajero
-      cantBilletesReq = dineroRestante/billete; // Catidad de billetes requeridos
-      dineroRestante = dineroRestante%billete; // Dinero restante por dar al cliente
+      cantBilletesReq = (dineroRestante-(dineroRestante%billete))/billete; // Catidad de billetes requeridos
 
-      if (dineroRestante == 0) {
-        todoOK = true;
+      if(cantBilletes>0) {
+        cantBilletesUsad.push(cantBilletesReq); // Almacena las cantidades de billetes usados para concretar la operación
+        dineroRestante = dineroRestante%billete; // Dinero restante por dar al cliente
+        if (dineroRestante == 0) {
+          todoOK = true;
+          this.modificarCantBilletes(cantBilletesUsad);
+        }
       }
       i++;
     }
+    // Cuando da todo OK AHí TENGO QUE CAMBIAR LA CANTIDAD DE BILLETES.
     this.mostrarMsj(todoOK);
   }
 
@@ -34,7 +50,7 @@ class Cajero {
     if(todoOK) {
       console.log("EXCELENTE :D tenga su dinero");
     }else {
-      console.log("ERROR");
+      console.log("No puedo darte la cantidad que me estas pidiendo.");
     }
   }
 }
