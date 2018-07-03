243df2cee5c5887bb527a9a3c11cc3401ff203ce
German Fica <germafica@gmail.com>
2018-07-03 19:05:25 -0300

    New update

--- clase_22/js/principal.js
+++ clase_22/js/principal.js
@@ -6,30 +6,42 @@ var billetes = [
 
 function retirar(monto) {
   // Declaración de variables
-  var i;
+  var i, billete, cantBilletes, cantBilletesReq, dineroRestante, todoOK;
 
   // Inicialización de variables
   i=0;
+  todoOK = false;
 
-  while (i<=billetes.length-1) {
-    var billete = billetes[i][0];
-    var cantBilletes = billetes[i][1];
-    var cantBilletesNec = monto/billete; // Catidad de billetes requeridos
+  while (i<=billetes.length-1 && !todoOK) {
+    billete = billetes[i][0]; // Billete actual del cajero
+    cantBilletes = billetes[i][1]; // Cantidad de billetes del cajero
+    cantBilletesReq = monto/billete; // Catidad de billetes requeridos
+    dineroRestante = monto%billete; // Dinero restante por dar al cliente
 
     console.log(monto); // Monto que seleccionó el cliente para retirar
-    console.log(monto/billete);
-    console.log(monto%billete); // Dinero restante por dar al cliente
+    console.log(cantBilletesReq);
+    console.log(dineroRestante);
 
     // Si la cantidad de biletes requeridos
     // es menor a la
-    //cantidad de billetes en el cajero,
+    // cantidad de billetes en el cajero,
     // entonces ahora el monto será el dinero restante para completar el
     // monto seleccionado
-    if(cantBilletes>=cantBilletesNec) {
-      monto = monto%billete; // Dinero restante por dar al cliente
+    if(cantBilletes>=cantBilletesReq) {
+      monto = dineroRestante; // Dinero restante por dar al cliente
+    }
+
+    if (monto == 0) {
+      todoOK = true;
     }
 
     //console.log(cantBilletes + " billetes de " + billete + ".")
     i++;
+  }
+
+  if(todoOK) {
+    console.log("EXCELENTE :D tenga su dinero");
+  }else {
+    console.log("ERROR");
   }
 }
