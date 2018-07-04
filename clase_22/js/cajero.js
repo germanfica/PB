404b95a9b0db4c6c9a9716564925778944174c13
German Fica <germafica@gmail.com>
2018-07-03 23:57:12 -0300

    New update

--- clase_22/js/cajero.js
+++ clase_22/js/cajero.js
@@ -17,22 +17,6 @@ class Cajero {
     }
   }
 
-  todaviaNoLoUso() {
-    // Si hay billetes en el cajero entonces
-    if(this.billetes[i][1]>0) {
-      billetes.push(cantidadNecesaria); // Cantidad de billetes necesarios para dar al cliente
-      monto = monto%this.billetes[i][0]; // Monto restante para completar el monto que solicitó el cliente
-      // Si todo salió bien entonces dar los billetes al cliente
-      if (monto == 0) {
-        todoOK = true;
-        this.darBilletes(billetes);
-      }
-    }
-
-    // Cuando da todo OK AHí TENGO QUE CAMBIAR LA CANTIDAD DE BILLETES.
-    //this.mostrarMsj(todoOK);
-  }
-
   /**
    * Determina la cantidad de billetes necesarios para darle al cliente
    * en base a los billetes que hay en el cajero
