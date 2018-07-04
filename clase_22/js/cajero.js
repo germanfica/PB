3d12bcdf7502b49ff1f1e2274b96f4a3edb5e527
German Fica <germafica@gmail.com>
2018-07-04 00:21:01 -0300

    New update Ahora sí, todo funcional y bien docuentado

--- clase_22/js/cajero.js
+++ clase_22/js/cajero.js
@@ -8,19 +8,19 @@ class Cajero {
   }
 
   /**
-   * Dar al cliente el monto solicitado
+   * Dar al cliente el monto que solicitó
    */
   darBilletes(billetes) {
     for (var i = 0; i <= billetes.length-1; i++) {
-      if(this.billetes[i][1]>0)
+      if(this.billetes[i][1]>0) {
         this.billetes[i][1] = (this.billetes[i][1]) - (billetes[i]);
+      }
     }
   }
 
   /**
-   * Determina la cantidad de billetes necesarios para darle al cliente
-   * en base a los billetes que hay en el cajero
-   * Cambiar nombre a seNecesitan
+   * Determina la cantidad de billetes que se necesitan
+   * para darle al cliente
    */
   seNecesitan(monto) {
     // Declaración de variables
@@ -29,23 +29,28 @@ class Cajero {
 
     for (var i = 0; i <= this.billetes.length-1; i++) {
       cantidadNecesaria = (monto-(monto%this.billetes[i][0]))/this.billetes[i][0]; // Catidad de billetes requeridos
-      billetes.push(cantidadNecesaria); // Cantidad de billetes necesarios para dar al cliente
-      monto = monto%this.billetes[i][0]; // Monto restante para completar el monto que solicitó el cliente
+      billetes.push(cantidadNecesaria); // Cantidad de billetes necesarios para darle al cliente
+      monto = monto%this.billetes[i][0]; // Monto restante para completar el monto total solicitado por el cliente
     }
 
     return billetes;
   }
 
   hayBilletes(billetes) {
-    var respuesta;
+    // Declaración de variables
+    var i, respuesta;
 
-    for (var i = 0; i <= billetes.length-1; i++) {
-      if(this.billetes[i][1]>=billetes[i]) {
-        respuesta = true;
-      }else {
+    // Inicialización de variables
+    respuesta = true;
+    i = 0;
+
+    while(i<= billetes.length-1 && respuesta) {
+      if(this.billetes[i][1]<=0) {
         respuesta = false;
       }
+      i++;
     }
+
     return respuesta;
   }
 
@@ -55,6 +60,8 @@ class Cajero {
 
     // Inicialización de variables
     billetes = this.seNecesitan(monto);
+    console.log(this.billetes);
+    //console.log(billetes);
 
     if(this.hayBilletes(billetes)) {
       console.log("EXCELENTE :D tenga su dinero.");
