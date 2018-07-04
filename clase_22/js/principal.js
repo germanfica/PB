7dd01327f5623427dbee1d736f1d5a722a286280
German Fica <germafica@gmail.com>
2018-07-04 13:54:01 -0300

    New update

--- clase_22/js/principal.js
+++ clase_22/js/principal.js
@@ -1,2 +1,10 @@
 // Abrir un cajero
 var cajero = new Cajero();
+
+var button = document.getElementById('id_boton');
+button.addEventListener('click', botonClick);
+
+function botonClick(evento) {
+  var monto = document.getElementById('id_monto');
+  cajero.retirar(monto.value);
+}
